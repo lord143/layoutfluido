ff932fb6b13d6384712e56aae960cc18d43cec9a
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-05-21 10:40:58 -0300

    [FIX] - começando pagina com layout fluido

--- src/App.js
+++ src/App.js
@@ -5,30 +5,33 @@ import Global from "./styles/global";
 import Home from "./pages/Home/Home";
 
 const lorem =
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed iure blanditiis voluptatum nulla quidem minus quam tempora obcaecati necessitatibus inventore! Vitae totam quam pariatur facilis fugit maxime adipisci eaque.";
+  "A beleza está em todos os lugares.";
+
+const dev = 
+  "DEV em progresso, codando em seu tempo"
 
 const data = [
   {
     id: Math.random(),
-    title: "Box titulo 1",
+    title: dev,
     text: lorem,
     bgColor: "#D5CAFA"
   },
   {
     id: Math.random(),
-    title: "Box titulo 2",
+    title: dev,
     text: lorem,
     bgColor: "#EDA9A9"
   },
   {
     id: Math.random(),
-    title: "Box titulo 3",
+    title: dev,
     text: lorem,
     bgColor: "#F2EE8D"
   },
   {
     id: Math.random(),
-    title: "Box titulo 4",
+    title: dev,
     text: lorem,
     bgColor: "#9FEACD"
   }
